2d72a3d0ec8e1fd89004398ce68845f0d13bdb8c
Ken Southerland <crowmagnumb@gmail.com>
2020-06-28 12:41:57 -0700

    new webpack has alias issue

--- src/app/projects/myproject/sandbox.module.platform.tns.ts
+++ src/app/projects/myproject/sandbox.module.platform.tns.ts
@@ -1,12 +1,14 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
+import {
+    NativeScriptModule,
+    NativeScriptRouterModule,
+} from "@nativescript/angular";
 
 import { routes } from "./sandbox.routes";
 
 export const platform = {
     imports: [NativeScriptModule, NativeScriptRouterModule.forChild(routes)],
     schemas: [NO_ERRORS_SCHEMA],
-    declarations: []
+    declarations: [],
 };
